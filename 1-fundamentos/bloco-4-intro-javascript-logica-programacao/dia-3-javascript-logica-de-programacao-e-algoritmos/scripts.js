4341eccf73694a5ad02405794a4b51474aeec773
Pedro <pedro_hasse@yahoo.com.br>
2022-05-17 15:49:15 -0300

    Montada toda a estrutura dos exercicios do dia, resolução pra depois

--- 1-fundamentos/bloco-4-intro-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ 1-fundamentos/bloco-4-intro-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -127,7 +127,7 @@ ________________________________________________________________________________
 // console.log(fatorial);
 
 
-//  FORMA REFATORADA
+// //  FORMA REFATORADA
 // let fatorando = 10; // variavel que indica o número a ser fatorado
 // let fatorial = 1; // variavel que seta o início da multiplicação ?
 
@@ -153,3 +153,81 @@ ________________________________________________________________________________
 //   }
 //   console.log(drow);
 // }
+
+// EXERCICIO 2 - Take two
+// Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber'; //variavel que contem a palavra a ser invertida
+let drow = ''; //variavel que vai receber a palavra já invertida
+
+
+
+// EXERCICIO 3
+// 3- Considere o array de strings abaixo:
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+
+// EXERCICIO 4
+// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+
+
+// BÔNUS
+// 1- Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// 3- Agora inverta o lado do triângulo. Por exemplo:
+
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+
+// n = 5
+
+//   *
+//  ***
+// *****
+
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+
+// Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+// n = 7
+
+//    *
+//   * *
+//  *   *
+// *******
+
+
+// 6- Faça um programa que diz se um número definido numa variável é primo ou não.
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+// Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
