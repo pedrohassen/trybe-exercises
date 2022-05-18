8791bd8b537face256acbc82d648efe28b27dd60
Pedro <pedro_hasse@yahoo.com.br>
2022-05-18 02:15:05 -0300

    Resolução de todos os exercícios do dia

--- 1-fundamentos/bloco-4-intro-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ 1-fundamentos/bloco-4-intro-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -157,9 +157,22 @@ ________________________________________________________________________________
 // EXERCICIO 2 - Take two
 // Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'tryber'; //variavel que contem a palavra a ser invertida
-let drow = ''; //variavel que vai receber a palavra já invertida
+// let word = 'tryber'; //variavel que contem a palavra a ser invertida
+// let drow = ''; //variavel que vai receber a palavra já invertida
 
+// for (let index = 0; index < word.length; index += 1) {
+//   drow += word[word.length - 1 - index];
+// }
+
+// console.log(drow);
+
+
+// let word = 'tryber';
+// let reverseWord = '';
+
+// reverseWord = word.split('').reverse().join('');
+
+// console.log(reverseWord);
 
 
 // EXERCICIO 3
@@ -169,13 +182,50 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let biggestWord = array[0];
+// let smallestWord = array[0];
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length) {
+//     biggestWord = array[index];
+//   }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length < smallestWord.length) {
+//     smallestWord = array[index];
+//   }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
 
 // EXERCICIO 4
 // 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
 
+// Números primos são divisíveis por 1 e por eles mesmos apenas.
+
+// let biggestPrimeNumber = 0;
+
+// for (let currentNumber = 0 ; currentNumber <= 50; currentNumber += 1) {
+//   let isPrime = true;
+//   for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+//     if (currentNumber % currentDivisor === 0) {
+//       isPrime = false;
+//     }
+//   }
+//   if (isPrime) {
+//     biggestPrimeNumber = currentNumber;
+//   }
+// }
+
+// console.log(biggestPrimeNumber)
 
 
-// BÔNUS
+// BÔNUS - // ESTUDAR MAIS SOBRE ESSE
 // 1- Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
 
 // n = 5
@@ -186,6 +236,21 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 // *****
 // *****
 
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   inputLine = inputLine + symbol;
+// };
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   console.log(inputLine);
+// };
+
+
+// ESTUDAR MAIS SOBRE ESSE
+
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
 // n = 5
@@ -195,6 +260,19 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 // ***
 // ****
 // *****
+
+
+// let size = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+//   console.log(inputLine);
+//   inputLine = inputLine + symbol;
+// };
+
+
+// ESTUDAR MAIS SOBRE ESSE
 
 // 3- Agora inverta o lado do triângulo. Por exemplo:
 
@@ -208,6 +286,27 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 
 // Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
 
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+// let inputPosition = n;
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex < inputPosition) {
+//       inputLine = inputLine + ' ';
+//     } else {
+//       inputLine = inputLine + symbol;
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   inputPosition -= 1;
+// };
+
+
+
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
 
 // n = 5
@@ -215,6 +314,31 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 //   *
 //  ***
 // *****
+
+
+// ESTUDAR MAIS SOBRE ESSE
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// let midOfMatrix = (n + 1) / 2;
+// let controlLeft = midOfMatrix;
+// let controlRight = midOfMatrix;
+
+// for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex > controlLeft && columnIndex < controlRight) {
+//       inputLine = inputLine + symbol;
+//     } else {
+//       inputLine = inputLine + ' ';
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   controlRight += 1;
+//   controlLeft -= 1
+// };
 
 
 // 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
@@ -228,6 +352,38 @@ let drow = ''; //variavel que vai receber a palavra já invertida
 // *******
 
 
+// ESTUDAR MAIS SOBRE ESSE
+
+// let n = 7;
+// let middle = (n + 1) / 2;
+// let controlLeft = middle;
+// let controlRight = middle;
+// let symbol = '*';
+// for (let line = 1; line <= middle; line += 1) {
+//   let outputLine = '';
+//   for (let col = 1; col <= n; col += 1) {
+//     if (col == controlLeft || col == controlRight || line == middle) {
+//       outputLine += symbol;
+//     } else {
+//       outputLine += ' ';
+//     }
+//   }
+//   controlLeft -= 1;
+//   controlRight += 1;
+//   console.log(outputLine);
+// }
+
 // 6- Faça um programa que diz se um número definido numa variável é primo ou não.
 // Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
 // Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+
+let divisors = 1;
+let numberToCheck = 31;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
