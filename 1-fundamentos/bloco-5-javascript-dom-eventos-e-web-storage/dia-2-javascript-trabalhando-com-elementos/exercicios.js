bc2b5f09aaa0706e0028e66d333de9425a16e4e4
Pedro <pedro_hasse@yahoo.com.br>
2022-05-20 21:43:26 -0300

    Exercicio 6

--- 1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
+++ 1-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
@@ -35,6 +35,13 @@ elementMain.appendChild(elementLeftSection);
 console.log(elementLeftSection);
 
 // 6. 🚀 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+const elementRightSection = document.createElement('section');
+elementRightSection.className = 'right-content';
+elementMain.appendChild(elementRightSection);
+
+console.log(elementRightSection);
+
 // 7. 🚀 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
 // 8. 🚀 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // 9. 🚀 Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
