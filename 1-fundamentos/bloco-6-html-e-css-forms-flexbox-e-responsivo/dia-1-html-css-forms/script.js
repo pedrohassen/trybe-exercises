e05d3e5060bc085ab5efc00a5c58994a2216758c
Pedro <pedro_hasse@yahoo.com.br>
2022-06-01 16:33:04 -0300

    Exercicios do dia, pequena alteração

--- 1-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ 1-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,7 @@
 const BTN_SUBMIT = document.querySelector('#btn1');
 
-BTN_SUBMIT.addEventListener('click', (e) => {
+window.onload = () => {
+  BTN_SUBMIT.addEventListener('click', (e) => {
   e.preventDefault();
 });
+}
