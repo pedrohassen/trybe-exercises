9ed548b2fca57f11dac0fcb9015dfa31a923f2a3
Pedro <pedro_hasse@yahoo.com.br>
2022-06-22 18:51:45 -0300

    Adicionando conteúdo do dia

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/High Order Functions.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#signup-button');
+
+const registerUser = () => {
+  console.log('Registrado com sucesso!');
+};
+
+button.addEventListener('click', registerUser); // HoFs são: funções que usam outras funções, Ex.: o AddEventListener.
