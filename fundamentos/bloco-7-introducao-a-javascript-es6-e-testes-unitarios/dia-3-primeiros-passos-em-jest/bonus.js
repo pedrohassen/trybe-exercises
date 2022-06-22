794911037173da58b753c457154965d75b3a4fa2
Pedro <pedro_hasse@yahoo.com.br>
2022-06-21 23:48:43 -0300

    Exercicios do dia 7.3

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/bonus.js
@@ -0,0 +1,5 @@
+// Bônus
+// 🚀 1 - Você está pronto para um desafio?! Neste exercício, você irá praticar o desenvolvimento orientado a testes implementando:
+// A função;
+// Os testes para essa função;
+// Considere os dados abaixo. Você deve criar uma função que receba dois parâmetros: o id do funcionário e a informação disponível sobre ele (firstName, lastName, specialities). Você também deverá criar os testes para essa função. Sua função deverá então retornar os resultados da busca pelo id para aquele funcionário e a informação consultada. Caso o id não conste no quadro de funcionários, sua função deve retornar o erro "ID não identificada". Se a informação que se quer acessar não existir, a função deve retornar o erro "Informação indisponível".
